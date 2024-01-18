a996335e8f5ca9f315f9f2f6d6ba07df138003a6
vasileios20 <vasilis_tsim@outlook.com>
2024-01-18 09:20:13 +0000

    Create profile app.Add serializer and list view

--- re_drf_api/settings.py
+++ re_drf_api/settings.py
@@ -45,6 +45,9 @@ INSTALLED_APPS = [
     "cloudinary_storage",
     "django.contrib.staticfiles",
     "cloudinary",
+    "rest_framework",
+    "profiles",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
