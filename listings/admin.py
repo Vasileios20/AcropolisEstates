2b59abeffea443dac5ff8347e900f60464f29065
vasileios20 <vasilis_tsim@outlook.com>
2024-02-10 13:02:11 +0000

    Add approve function, list display/filter and search fields to listings admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from listings.models import Listing, Images
 
-admin.site.register(Listing)
-admin.site.register(Images)
 
-# Register your models here.
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("type", "sale_type", "price", "approved", "owner")
+    list_filter = ("owner", "type", "sale_type", "price", "approved")
+    search_fields = ("type", "description", "city", "price", "owner", "sale_type")
+    list_per_page = 25
+    actions = ["approve_listings"]
+
+    def approve_listings(self, request, queryset):
+        queryset.update(approved=True)
+
+
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Images)
