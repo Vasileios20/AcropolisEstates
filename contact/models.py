4b734662ed8d052e3dabbc421b8be9961beb29f6
vasileios20 <vasilis_tsim@outlook.com>
2024-04-19 08:43:38 +0100

    Update contact form fields first and last name and phone_number

--- contact/models.py
+++ contact/models.py
@@ -11,9 +11,9 @@ class ContactForm(models.Model):
     subject = models.CharField(max_length=255)
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-    first_name = models.CharField(max_length=255, default="")
-    last_name = models.CharField(max_length=255, default="")
-    phone_number = models.CharField(max_length=255, default="")
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
 
     class Meta:
         ordering = ["-created_at"]
