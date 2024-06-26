d2377aa4c3dafaff468c1149129827a715b89a3a
vasileios20 <vasilis_tsim@outlook.com>
2024-06-26 15:21:10 +0100

    feat: Update email backend configuration to use Office 365 SMTP server
    This commit updates the email backend configuration in the settings file to use the Office 365 SMTP server instead of the previous Mailgun server. This change is made to improve email delivery reliability and ensure compatibility with the client's email infrastructure.

--- re_drf_api/settings.py
+++ re_drf_api/settings.py
@@ -81,7 +81,7 @@ if "CLIENT_ORIGIN_DEV" in os.environ:
 CORS_ALLOW_CREDENTIALS = True
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.eu.mailgun.org'
+EMAIL_HOST = 'smtp.office365.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
