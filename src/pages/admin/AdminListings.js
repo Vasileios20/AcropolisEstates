ee06f7cd09c7b6e7f6e4886d9079f30a2fe011d5
vasileios20 <vasilis_tsim@outlook.com>
2024-09-12 12:35:09 +0100

    Refactor AdminListings component: Use useFetchAllListings hook for fetching all listings and implement sorting functionality

--- src/pages/admin/AdminListings.js
+++ src/pages/admin/AdminListings.js
@@ -1,59 +1,89 @@
-import React from 'react'
-import useFetchListings from '../../hooks/useFetchListings';
+import React, { useState } from 'react'
+import useFetchAllListings from '../../hooks/useFetchAllListings';
 import styles from '../../styles/Admin.module.css';
 import amenitiesStyles from "../../styles/Listing.module.css";
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import { useHistory } from 'react-router-dom';
+import Asset from '../../components/Asset';
 
 export default function AdminListings() {
 
-    const { listings, setListings, hasLoaded } = useFetchListings();
-
+    const { listings, setListings, hasLoaded } = useFetchAllListings();
+    const [sortConfig, setSortConfig] = useState({ key: 'id', direction: 'asc' });
     const history = useHistory();
 
     const handleRowClick = (id) => {
         history.push(`/listings/${id}`);
     }
 
+    const handleSort = (sortType) => {
+        let sortedListings = [...listings.results]; // Clone the array
+
+        const direction = sortConfig.key === sortType && sortConfig.direction === 'asc' ? 'desc' : 'asc';
+
+        sortedListings.sort((a, b) => {
+            if (sortType === 'price' || sortType === 'id' || sortType === 'approved' || sortType === 'featured') {
+                // Sorting numeric or boolean values
+                return direction === 'asc' ? a[sortType] - b[sortType] : b[sortType] - a[sortType];
+            } else {
+                // Sorting string values (agent_name, type, sub_type, sale_type)
+                return direction === 'asc'
+                    ? a[sortType].localeCompare(b[sortType])
+                    : b[sortType].localeCompare(a[sortType]);
+            }
+        });
+
+        // Update the listings state with the sorted results
+        setListings({ results: sortedListings });
+        // Update the sort config state
+        setSortConfig({ key: sortType, direction });
+    }
 
 
     return (
         <Container fluid>
             <Row className="mx-2">
-                <div className={`${styles.Admin}`}>
-                    <h1>Listings</h1>
-                    <Table>
-                        <thead>
-                            <tr>
-                                <th>Agent Name</th>
-                                <th>ID</th>
-                                <th>Type</th>
-                                <th>Sub Type</th>
-                                <th>Sale Type</th>
-                                <th>Price</th>
-                                <th>Approved</th>
-                                <th>Featured</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {listings.results.map(listing => (
-                                <tr key={listing.id} onClick={() => handleRowClick(listing.id)} style={{ cursor: 'pointer' }}>
-                                    <td>{listing.agent_name}</td>
-                                    <td>{listing.id}</td>
-                                    <td>{listing.type}</td>
-                                    <td>{listing.sub_type}</td>
-                                    <td>{listing.sale_type}</td>
-                                    <td>{listing.price}</td>
-                                    {listing.approved ? <td><i className={`fa-solid fa-square-check ${amenitiesStyles.AmenityChecked}`}></i></td> : <td></td>}
-                                    {listing.featured ? <td><i className={`fa-solid fa-square-check ${amenitiesStyles.AmenityChecked}`}></i></td> : <td></td>}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </Table>
-                </div>
-
+                <>
+                    <div className={`${styles.Admin}`}>
+                        <h1 className="text-center">Listings</h1>
+                        {hasLoaded ?
+                            <>
+                                <Table>
+                                    <thead>
+                                        <tr>
+                                            <th onClick={() => handleSort('agent_name')} style={{ cursor: 'pointer' }}>Agent Name</th>
+                                            <th onClick={() => handleSort('id')} style={{ cursor: 'pointer' }}>ID</th>
+                                            <th onClick={() => handleSort('type')} style={{ cursor: 'pointer' }}>Type</th>
+                                            <th onClick={() => handleSort('sub_type')} style={{ cursor: 'pointer' }}>Sub Type</th>
+                                            <th onClick={() => handleSort('sale_type')} style={{ cursor: 'pointer' }}>Sale Type</th>
+                                            <th onClick={() => handleSort('price')} style={{ cursor: 'pointer' }}>Price</th>
+                                            <th onClick={() => handleSort('approved')} style={{ cursor: 'pointer' }}>Approved</th>
+                                            <th onClick={() => handleSort('featured')} style={{ cursor: 'pointer' }}>Featured</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {listings.results.map(listing => (
+                                            <tr key={listing.id} onClick={() => handleRowClick(listing.id)} style={{ cursor: 'pointer' }}>
+                                                <td>{listing.agent_name}</td>
+                                                <td>{listing.id}</td>
+                                                <td>{listing.type}</td>
+                                                <td>{listing.sub_type}</td>
+                                                <td>{listing.sale_type}</td>
+                                                <td>{listing.price}</td>
+                                                {listing.approved ? <td><i className={`fa-solid fa-square-check ${amenitiesStyles.AmenityChecked}`}></i></td> : <td></td>}
+                                                {listing.featured ? <td><i className={`fa-solid fa-square-check ${amenitiesStyles.AmenityChecked}`}></i></td> : <td></td>}
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </Table>
+                            </>
+                            : <><Asset spinner />
+                            </>
+                        }
+                    </div>
+                </>
             </Row>
         </Container>
     )
