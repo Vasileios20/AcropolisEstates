383e346044b89d7ff3f18a5dd90d53c9acf48d02
vasileios20 <vasilis_tsim@outlook.com>
2024-09-29 18:39:26 +0100

    Refactor ContactFormAdmin in contact/admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import ContactForm
 
 
-admin.site.register(ContactForm)
+class ContactFormAdmin(admin.ModelAdmin):
+    list_display = ("id", "first_name", "last_name", "email",
+                    "phone_number", "subject", "created_on")
+    list_filter = ("first_name", "last_name", "email",
+                   "phone_number", "subject", "created_on")
+    search_fields = ("first_name", "last_name", "email",
+                     "phone_number", "subject", "created_on")
+    list_per_page = 50
+
+
+admin.site.register(ContactForm, ContactFormAdmin)
