5a4ae983484277a8fd61b2b70db3e316fd8f40cd
vasileios20 <vasilis_tsim@outlook.com>
2025-01-01 15:41:08 +0000

    feat: Enhance ListingsPage to include fetchListings function and improve message handling for search results

--- frontend/src/pages/listings/ListingsPage.js
+++ frontend/src/pages/listings/ListingsPage.js
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 function ListingsPage({ nonEssentialConsent, setShowCookieBanner }) {
-  const { listings, setListings, hasLoaded } = useFetchListings();
+  const { listings, setListings, hasLoaded, fetchListings } = useFetchListings();
   const { state } = useLocation();
   const [searchResults, setSearchResults] = useState(false);
   const message = "No results";
@@ -12,14 +12,18 @@ function ListingsPage({ nonEssentialConsent, setShowCookieBanner }) {
   useEffect(() => {
     if (state?.data) {
       setSearchResults(true);
-      const approvedListings = state.data.results.filter(listing => listing.approved);
-      setListings({ results: approvedListings.length ? approvedListings : [], message });
-    } else {
-      setListings(listings);
-    }
-  }, [setListings, state, listings]);
 
-  const displayListings = state ? state.data.results.filter(listing => listing.approved) : listings.results;
+      const approvedListings = state.data.results.filter((listing) => listing.approved);
+      setListings({
+        results: approvedListings.length ? approvedListings : [],
+        message: approvedListings.length ? undefined : message,
+      });
+    } else {
+      fetchListings();
+    }
+  }, [state, setListings, fetchListings, message]);
+
+  const displayListings = state?.data?.results.filter((listing) => listing.approved) ?? listings.results;
 
   return (
     <ListingsComponent
