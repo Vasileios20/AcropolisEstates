50355fa423c92f970d3974ab27bf9db839de1f6c
vasileios20 <vasilis_tsim@outlook.com>
2025-01-07 19:13:35 +0000

    feat: add CustomDropdown component for listing type selection in search bar

---
+++ frontend/src/components/searchBar/TypeDropDown.js
@@ -0,0 +1,38 @@
+import Dropdown from "react-bootstrap/Dropdown";
+import styles from "../../styles/SearchBar.module.css";
+import { t } from "i18next";
+
+const CustomDropdown = ({ type, setType }) => {
+    const typeCapitalized = type?.replace(/\b\w/g, char => char.toUpperCase());
+    const options = [
+        { value: "", label: t("listingType.any") },
+        { value: "residential", label: t("listingType.residential") },
+        { value: "land", label: t("listingType.land") },
+        { value: "commercial", label: t("listingType.commercial") },
+    ];
+
+    const handleSelect = (value) => {
+        setType(value);
+    };
+
+    return (
+        <Dropdown className="w-100">
+            <Dropdown.Toggle className={`${styles.TypeSelect} text-start`} id="dropdown-basic">
+                {typeCapitalized || t("listingType.any")}
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu className={styles.TypeDropdown}>
+                {options.map((option) => (
+                    <Dropdown.Item
+                        key={option.value}
+                        onClick={() => handleSelect(option.value)}
+                    >
+                        {t(`listingType.${option.value}`)}
+                    </Dropdown.Item>
+                ))}
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+};
+
+export default CustomDropdown;
