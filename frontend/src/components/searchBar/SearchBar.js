df1fc76d156cfa0a70df4956fc466f533948f8a6
vasileios20 <vasilis_tsim@outlook.com>
2025-01-09 14:59:00 +0000

    feat: implement new search algorithm for improved performance and accuracy

--- frontend/src/components/searchBar/SearchBar.js
+++ frontend/src/components/searchBar/SearchBar.js
@@ -3,21 +3,16 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
 import Alert from "react-bootstrap/Alert";
-import Tooltip from "react-bootstrap/Tooltip";
-import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 import styles from "../../styles/SearchBar.module.css";
-import btnStyles from "../../styles/Button.module.css";
 
 import { axiosReq } from "../../api/axiosDefaults";
 import { useHistory } from "react-router-dom";
-
-import { useTranslation } from "react-i18next";
 import useFetchLocationData from "../../hooks/useFetchLocationData";
-import MunicipalitySearch from "./MunicipalitySearch";
-import TypeDropDown from "./TypeDropDown";
 import AdvancedFiltersModal from "./AdvancedFiltersModal";
+import MainSearchFields from "./MainSearchFields";
+import { SaleTypeSearch } from "./SaleTypeSearch";
+import { ButtonsSearch } from "./ButtonsSearch";
 
 const SearchBar = () => {
   /**
@@ -27,15 +22,14 @@ const SearchBar = () => {
    * @returns {JSX.Element} - The JSX for the component.
   */
 
-  const [saleType, setSaleType] = useState("");
-  const [type, setType] = useState("");
-  const [price, setPrice] = useState({ min: "", max: "" });
-  const [surface, setSurface] = useState({ min: "", max: "" });
-  const [regionId, setRegionId] = useState("");
-  const [countyId, setCountyId] = useState("");
-  const [municipalityId, setMunicipalityId] = useState("");
-
   const [filters, setFilters] = useState({
+    saleType: "",
+    type: "",
+    price: { min: "", max: "" },
+    surface: { min: "", max: "" },
+    regionId: "",
+    countyId: "",
+    municipalityId: "",
     amenities: [],
     bedrooms: { min: "", max: "" },
     constructionYear: { min: "", max: "" },
@@ -47,49 +41,27 @@ const SearchBar = () => {
   const location = history.location;
   const [errors, setErrors] = useState("");
   const [empty, setEmpty] = useState(false);
-
-  const renderTooltip = (props) => (
-    <Tooltip id="tooltip-disabled" {...props}>
-      Please choose rent or buy.
-    </Tooltip>
-  );
-
-  const { t } = useTranslation();
   const { regionsData } = useFetchLocationData();
-
-
-
 
   // Fetch the search parameters from the URL and set the state.
   useMemo(() => {
     const search = history.location.search;
 
     const params = new URLSearchParams(search);
-    const saleType = params.get("sale_type");
-    const type = params.get("type");
-    const minPrice = params.get("min_price");
-    const maxPrice = params.get("max_price");
-    const minSurface = params.get("min_surface");
-    const maxSurface = params.get("max_surface");
-    const regionId = params.get("region_id");
-    const countyId = params.get("county_id");
-    const municipalityId = params.get("municipality_id" || "");
-    const amenities = params.getAll('amenities') || [];
     const filters = {
-      amenities: amenities || [],
+      saleType: params.get("sale_type"),
+      type: params.get("type"),
+      price: { min: params.get("min_price"), max: params.get("max_price") },
+      surface: { min: params.get("min_surface"), max: params.get("max_surface") },
+      regionId: params.get("region_id"),
+      countyId: params.get("county_id"),
+      municipalityId: params.get("municipality_id"),
+      amenities: params.getAll('amenities') || [],
       bedrooms: { min: params.get('min_bedrooms'), max: params.get('max_bedrooms') },
       constructionYear: { min: params.get('min_construction_year'), max: params.get('max_construction_year') },
       floor: { min: params.get('min_floor'), max: params.get('max_floor') },
       heating_system: params.get('heating_system'),
     };
-
-    setSaleType(saleType);
-    setType(type);
-    setPrice({ min: minPrice, max: maxPrice });
-    setSurface({ min: minSurface, max: maxSurface });
-    setRegionId(regionId);
-    setCountyId(countyId);
-    setMunicipalityId(municipalityId);
     setFilters(filters);
     setUpdate(false);
   }, [history.location.search]);
@@ -98,27 +70,27 @@ const SearchBar = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     // Fetch the listings from the API using the search parameters.
-    let path = `/listings/?sale_type=${saleType}`;
-    if (municipalityId) {
-      path += `&region_id=${regionId}&county_id=${countyId}&municipality_id=${municipalityId}`;
+    let path = `/listings/?sale_type=${filters.saleType}`;
+    if (filters.municipalityId) {
+      path += `&region_id=${filters.regionId}&county_id=${filters.countyId}&municipality_id=${filters.municipalityId}`;
     }
     if (empty) {
-      path = `/listings/?sale_type=${saleType}`;
+      path = `/listings/?sale_type=${filters.saleType}`;
     }
-    if (type) {
-      path += `&type=${type}`;
+    if (filters.type) {
+      path += `&type=${filters.type}`;
     }
-    if (price.min) {
-      path += `&min_price=${price.min}`;
+    if (filters.price.min) {
+      path += `&min_price=${filters.price.min}`;
     }
-    if (price.max) {
-      path += `&max_price=${price.max}`;
+    if (filters.price.max) {
+      path += `&max_price=${filters.price.max}`;
     }
-    if (surface.min) {
-      path += `&min_surface=${surface.min}`;
+    if (filters.surface.min) {
+      path += `&min_surface=${filters.surface.min}`;
     }
-    if (surface.max) {
-      path += `&max_surface=${surface.max}`;
+    if (filters.surface.max) {
+      path += `&max_surface=${filters.surface.max}`;
     }
     if (filters.bedrooms.min) {
       (path += `&min_bedrooms=${filters.bedrooms.min}`);
@@ -164,13 +136,13 @@ const SearchBar = () => {
     const updateButtonLabel = () => {
       if (
         location.pathname === "/listings/" &&
-        (saleType ||
-          municipalityId ||
-          type ||
-          price.min ||
-          price.max ||
-          surface.min ||
-          surface.max)
+        (filters.saleType ||
+          filters.municipalityId ||
+          filters.type ||
+          filters.price.min ||
+          filters.price.max ||
+          filters.surface.min ||
+          filters.surface.max)
       ) {
         setUpdate(true);
       } else {
@@ -180,20 +152,31 @@ const SearchBar = () => {
     updateButtonLabel();
   }, [
     update,
-    municipalityId,
-    type,
-    price.min,
-    price.max,
-    surface.min,
-    surface.max,
-    saleType,
+    filters.municipalityId,
+    filters.type,
+    filters.price.min,
+    filters.price.max,
+    filters.surface.min,
+    filters.surface.max,
+    filters.saleType,
     location.pathname,
   ]);
 
   const handleMunicipalitySelect = (municipality) => {
-    setRegionId(municipality.region_id);
-    setCountyId(municipality.county_id);
-    setMunicipalityId(municipality.id);
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      regionId: municipality.region_id,
+      countyId: municipality.county_id,
+      municipalityId: municipality.id,
+    }));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [name]: value,
+    }));
   };
 
   const handleFilters = (filters) => {
@@ -207,171 +190,48 @@ const SearchBar = () => {
         onSubmit={handleSubmit}
       >
         <Row className="align-items-center justify-content-between ms-lg-5 mb-lg-0">
-          <Col sm={12} className="mb-1 d-flex align-items-center">
-            {errors &&
-              (setTimeout(() => setErrors(""), 3000),
-                (
-                  <Alert className={styles.ErrorWidth} variant="warning">
-                    {errors}
-                  </Alert>
-                ))}
-            <Button
-              className={saleType === "rent" ? `${btnStyles.AngryOcean} ${btnStyles.Button} me-2` : `${btnStyles.AngryOceanOutline} ${btnStyles.Button} me-2`}
-              onClick={() => setSaleType("rent")}
-            >
-              {t("searchBar.rent")}
-            </Button>
-            <Button
-              className={saleType === "sale" ? `${btnStyles.AngryOcean}  ${btnStyles.Button}` : `${btnStyles.AngryOceanOutline} ${btnStyles.Button}`}
-              onClick={() => setSaleType("sale")}
-            >
-              {t("searchBar.buy")}
-            </Button>
+          {errors &&
+            (setTimeout(() => setErrors(""), 3000),
+              (
+                <Alert className={styles.ErrorWidth} variant="warning">
+                  {errors}
+                </Alert>
+              ))}
+          <SaleTypeSearch
+            errors={errors}
+            setErrors={setErrors}
+            filters={filters}
+            setFilters={setFilters}
+            handleChange={handleChange} />
+          <Col xs={6} className="mb-1 d-flex align-items-center justify-content-end">
             <AdvancedFiltersModal
               onApplyFilters={handleFilters}
               filters={filters}
               setFilters={setFilters}
               handleSubmit={handleSubmit}
               update={update}
-            />
-          </Col>
-
-          <Col sm={6} md={3} className="my-1 my-md-0">
-            <Form.Label style={{ fontWeight: "500" }}>
-              {t("searchBar.location")}
-            </Form.Label>
-            <MunicipalitySearch
-              className={styles.SearchInput}
-              onSearch={handleMunicipalitySelect}
-              regionsData={regionsData}
-              history={history}
-              saleType={saleType}
               empty={empty}
               setEmpty={setEmpty}
+              regionsData={regionsData}
+              history={history}
+              handleMunicipalitySelect={handleMunicipalitySelect}
             />
           </Col>
-          <Col sm={6} md={2} className="mt-1 mt-md-0">
-            <Form.Label style={{ fontWeight: "500" }}>
-              {t("searchBar.type")}
-            </Form.Label>
-            <TypeDropDown type={type} setType={setType} />
-          </Col>
-
-          <Col lg={2} md={3} sm={6} className="mb-1">
-            <Form.Group as={Row} controlId="formGroupPrice">
-              <Form.Label column className="mb-0" style={{ fontWeight: "500" }}>
-                {t("searchBar.price")}
-              </Form.Label>
-              <Col sm={12} className="d-flex align-items-center">
-                <Form.Control
-                  className={styles.SearchInput}
-                  aria-label="min price"
-                  type="number"
-                  placeholder={t("searchBar.minPrice")}
-                  min="0"
-                  value={price.min ? price.min : ""}
-                  onChange={(e) => setPrice({ ...price, min: e.target.value })}
-                />
-                <Form.Control
-                  className={styles.SearchInput}
-                  aria-label="max price"
-                  type="number"
-                  placeholder={t("searchBar.maxPrice")}
-                  min={price.min ? price.min : "0"}
-                  max="10000000"
-                  value={price.max ? price.max : ""}
-                  onChange={(e) => setPrice({ ...price, max: e.target.value })}
-                />
-              </Col>
-            </Form.Group>
-          </Col>
-          <Col lg={2} md={3} sm={6} className="mb-2">
-            <Form.Group as={Row} controlId="formGroupSurface">
-              <Form.Label column className="mb-0" style={{ fontWeight: "500" }}>
-                {type === "land" ? t("searchBar.landArea") : t("searchBar.floorArea")}
-              </Form.Label>
-              <Col sm={12} className="d-flex align-items-center">
-                <Form.Control
-                  className={styles.SearchInput}
-                  aria-label="min surface"
-                  type="number"
-                  placeholder={t("searchBar.minFloorArea")}
-                  min="0"
-                  value={surface.min ? surface.min : ""}
-                  onChange={(e) => setSurface({ ...surface, min: e.target.value })}
-                />
-                <Form.Control
-                  className={styles.SearchInput}
-                  aria-label="max surface"
-                  type="number"
-                  placeholder={t("searchBar.maxFloorArea")}
-                  min={surface.min ? surface.min : "0"}
-                  max="1000000"
-                  value={surface.max ? surface.max : ""}
-                  onChange={(e) => setSurface({ ...surface, max: e.target.value })}
-                />
-              </Col>
-            </Form.Group>
-          </Col>
-
-
-          <Col xs={12} lg={3} className="mt-0 mt-md-1 mt-lg-4">
-            {!saleType ? (
-              <>
-                <OverlayTrigger
-                  placement="bottom"
-                  delay={{ show: 50, hide: 300 }}
-                  overlay={renderTooltip}
-                >
-                  <span className="d-inline-block">
-                    <Button
-                      className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn} me-2`}
-                      type="submit"
-                      style={{ pointerEvents: 'none' }}
-                    >
-                      {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
-                    </Button>
-                  </span>
-                </OverlayTrigger>
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Remove}`}
-                  onClick={() => {
-                    setSaleType("");
-                    setType("");
-                    setPrice({ min: "", max: "" });
-                    setSurface({ min: "", max: "" });
-                    setMunicipalityId("");
-                    setRegionId("");
-                    setCountyId("");
-                  }}>
-                  {t("searchBar.btnClear")}
-                </Button>
-
-              </>
-            ) : (
-              <>
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn} me-2`}
-                  type="submit"
-                >
-                  {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
-                </Button>
-                <Button
-                  className={`${btnStyles.Button} ${btnStyles.Remove}`}
-                  onClick={() => {
-                    setSaleType("");
-                    setType("");
-                    setPrice({ min: "", max: "" });
-                    setSurface({ min: "", max: "" });
-                    setMunicipalityId("");
-                    setRegionId("");
-                    setCountyId("");
-                  }}>
-                  {t("searchBar.btnClear")}
-                </Button>
-              </>
-            )}
-          </Col>
+          <MainSearchFields
+            filters={filters}
+            setFilters={setFilters}
+            onSearch={handleMunicipalitySelect}
+            regionsData={regionsData}
+            history={history}
+            empty={empty}
+            setEmpty={setEmpty}
+            handleChange={handleChange}
+          />
+          <ButtonsSearch
+            filters={filters}
+            setFilters={setFilters}
+            update={update}
+          />
         </Row>
       </Form>
     </Container>
