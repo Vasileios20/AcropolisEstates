a6452ff1624d96c2e6482630d5a0590426e55431
vasileios20 <vasilis_tsim@outlook.com>
2025-01-10 14:42:39 +0000

    feat: refactor ButtonsAdvancedFilters and ButtonsSearch components to use Bootstrap Row and Col for improved layout

--- frontend/src/components/searchBar/ButtonsAdvancedFilters.js
+++ frontend/src/components/searchBar/ButtonsAdvancedFilters.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
 import { t } from 'i18next'
 import btnStyles from '../../styles/Button.module.css'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
@@ -13,51 +15,54 @@ export const ButtonsAdvancedFilters = ({ filters, setFilters, update, handleAppl
     );
 
     return (
-        <>
-            <Button
-                className={`${btnStyles.Button} ${btnStyles.Remove} me-1`}
-                onClick={() => {
-                    setFilters({
-                        saleType: "",
-                        type: "",
-                        price: { min: "", max: "" },
-                        surface: { min: "", max: "" },
-                        regionId: "",
-                        countyId: "",
-                        municipalityId: "",
-                        amenities: [],
-                        bedrooms: { min: "", max: "" },
-                        constructionYear: { min: "", max: "" },
-                        floor: { min: "", max: "" },
-                    });
-                }}>
-                {t("searchBar.btnClear")}
-            </Button>
-            {!filters.saleType ? (
-                <>
-                    <OverlayTrigger
-                        placement="bottom"
-                        delay={{ show: 50, hide: 300 }}
-                        overlay={renderTooltip}
-                    >
-                        <span className="d-inline-block">
-                            <Button
-                                className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn}`}
-                                style={!filters.saleType && { pointerEvents: 'none', backgroundColor: '#4d6765' }}
-                                onClick={(e) => handleApply(e)}
-                                disabled={!filters.saleType}
-                            >
-                                {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
-                            </Button>
-                        </span>
-                    </OverlayTrigger>
-
-                </>
-            ) : (
-                <Button className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn}`} onClick={(e) => handleApply(e)} >
-                    {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
+        <><Row className="g-2">
+            <Col xs={12} className="mb-1 d-flex align-items-center justify-content-end">
+                <Button
+                    className={`${btnStyles.Button} ${btnStyles.Remove} me-1`}
+                    onClick={() => {
+                        setFilters({
+                            saleType: "",
+                            type: "",
+                            price: { min: "", max: "" },
+                            surface: { min: "", max: "" },
+                            regionId: "",
+                            countyId: "",
+                            municipalityId: "",
+                            amenities: [],
+                            bedrooms: { min: "", max: "" },
+                            constructionYear: { min: "", max: "" },
+                            floor: { min: "", max: "" },
+                        });
+                    }}>
+                    {t("searchBar.btnClear")}
                 </Button>
-            )}
+                {!filters.saleType ? (
+                    <>
+                        <OverlayTrigger
+                            placement="bottom"
+                            delay={{ show: 50, hide: 300 }}
+                            overlay={renderTooltip}
+                        >
+                            <span className="d-inline-block">
+                                <Button
+                                    className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn}`}
+                                    style={!filters.saleType && { pointerEvents: 'none', backgroundColor: '#4d6765', borderColor: '#4d6765' }}
+                                    onClick={(e) => handleApply(e)}
+                                    disabled={!filters.saleType}
+                                >
+                                    {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
+                                </Button>
+                            </span>
+                        </OverlayTrigger>
+
+                    </>
+                ) : (
+                    <Button className={`${btnStyles.Button} ${btnStyles.AngryOcean} ${btnStyles.SearchBtn}`} onClick={(e) => handleApply(e)} >
+                        {update ? t("searchBar.btnUpdate") : t("searchBar.btnSearch")}
+                    </Button>
+                )}
+            </Col>
+        </Row>
         </>
     )
 }
