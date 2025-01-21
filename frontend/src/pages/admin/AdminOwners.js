8e4c50fee7aaf305dc80f4967a59bf25614ee7fd
vasileios20 <vasilis_tsim@outlook.com>
2025-01-21 15:49:28 +0000

    feat: create AdminOwners component to display owner listings with file links

---
+++ frontend/src/pages/admin/AdminOwners.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import Table from 'react-bootstrap/Table';
+import styles from '../../styles/Admin.module.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import { useRedirect } from '../../hooks/useRedirect';
+import useUserStatus from '../../hooks/useUserStatus';
+import Forbidden403 from '../errors/Forbidden403';
+import { axiosReq } from '../../api/axiosDefaults';
+import { useTranslation } from 'react-i18next';
+
+const AdminOwners = () => {
+    useRedirect("loggedOut");
+    const userStatus = useUserStatus();
+    const [owners, setOwners] = useState([]);
+    const { t } = useTranslation();
+
+    useEffect(() => {
+        const fetchOwners = async () => {
+            try {
+                const response = await axiosReq.get('/listings/owners/');
+                console.log('response', response.data.results);
+
+                setOwners(response.data.results);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchOwners();
+    }, []);
+
+    if (userStatus === false) {
+        return <Forbidden403 />;
+    }
+
+    return (
+        <Container fluid>
+            <Row className="m-4">
+                <Table className={`${styles.Admin}`}>
+                    <thead>
+                        <tr>
+                            <th>{t('First Name')}</th>
+                            <th>{t('Last Name')}</th>
+                            <th>{t('Phone')}</th>
+                            <th>{t('Phone 2')}</th>
+                            <th>{t('Email')}</th>
+                            <th>{t('Notes')}</th>
+                            <th>{t('Files')}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {owners.map((owner, index) => (
+                            <tr key={index}>
+                                <td>{owner.first_name}</td>
+                                <td>{owner.last_name}</td>
+                                <td>{owner.phone}</td>
+                                <td>{owner.phone_2}</td>
+                                <td>{owner.email}</td>
+                                <td>{owner.notes}</td>
+                                <td>
+                                    <ul className={`list-unstyled ${styles.OwnerList}`}>
+                                        {owner.files.map((file, index) => (
+                                            <li key={index} className={`border rounded  p-1 mb-2 `}>
+                                                <a href={file.file_url} target="_blank" rel="noopener noreferrer">
+                                                    {file.file_url}
+                                                </a>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+            </Row>
+        </Container>
+    );
+}
+
+export default AdminOwners;
