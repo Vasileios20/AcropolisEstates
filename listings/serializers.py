f3e0431f18706ac8859983243f59230989b7adbc
vasileios20 <vasilis_tsim@outlook.com>
2025-01-24 18:09:24 +0000

    refactor: move generate_unique_filename function to utils.py for better organization

--- listings/serializers.py
+++ listings/serializers.py
@@ -3,18 +3,7 @@ from .models import Listing, Images, Amenities, Owner, OwnerFile
 from django.core.files.images import get_image_dimensions
 from .services import upload_to_backblaze
 from django.db.models import Max
-import uuid
-from datetime import datetime
-
-
-def generate_unique_filename(listing_id, idx):
-    """
-    Generate a unique filename for an image.
-    Combines listing ID, index, timestamp, and a UUID to ensure uniqueness.
-    """
-    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    unique_id = uuid.uuid4().hex[:8]
-    return f"listings/{listing_id}/image_{idx + 1}_{timestamp}_{unique_id}.jpg"
+from .utils import generate_unique_filename
 
 
 def validate_images(value):
