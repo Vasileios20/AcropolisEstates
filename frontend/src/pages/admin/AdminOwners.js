a78d2eb577a4161b02ae3a2fe57007c221fc4dc3
vasileios20 <vasilis_tsim@outlook.com>
2025-01-27 20:57:56 +0000

    fix: refactor AdminOwners component to use custom hook for fetching owners data

--- frontend/src/pages/admin/AdminOwners.js
+++ frontend/src/pages/admin/AdminOwners.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { useHistory } from 'react-router-dom';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
@@ -7,29 +7,19 @@ import Col from 'react-bootstrap/Col';
 import { useRedirect } from '../../hooks/useRedirect';
 import useUserStatus from '../../hooks/useUserStatus';
 import Forbidden403 from '../errors/Forbidden403';
-import { axiosReq } from '../../api/axiosDefaults';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import btnStyles from '../../styles/Button.module.css';
+import useFetchOwners from '../../hooks/useFetchOwners';
+import Asset from '../../components/Asset';
 
 const AdminOwners = () => {
     useRedirect("loggedOut");
     const userStatus = useUserStatus();
-    const [owners, setOwners] = useState([]);
     const { t } = useTranslation();
     const history = useHistory();
 
-    useEffect(() => {
-        const fetchOwners = async () => {
-            try {
-                const response = await axiosReq.get('/listings/owners/');
-                setOwners(response.data.results);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-        fetchOwners();
-    }, []);
+    const { owners, hasLoaded } = useFetchOwners();
 
     if (userStatus === false) {
         return <Forbidden403 />;
@@ -40,38 +30,44 @@ const AdminOwners = () => {
     }
 
     return (
-        <Container fluid style={{ paddingTop: '90px' }}>
-            <Row className="m-4">
-                <Col className=" d-flex justify-content-end g-0 mb-3">
-                    <Link to="/frontend/admin/listings/owners/create" className={`${btnStyles.AngryOcean} ${btnStyles.Button}`}>Add New Owner</Link>
-                </Col>
+        hasLoaded ? (
+            <Container fluid style={{ paddingTop: '90px' }} >
+                <Row className="m-4">
+                    <Col className=" d-flex justify-content-end g-0 mb-3">
+                        <Link to="/frontend/admin/listings/owners/create" className={`${btnStyles.AngryOcean} ${btnStyles.Button}`}>Add New Owner</Link>
+                    </Col>
 
-                <Table>
-                    <thead>
-                        <tr>
-                            <th>{t('First Name')}</th>
-                            <th>{t('Last Name')}</th>
-                            <th>{t('Phone')}</th>
-                            <th>{t('Phone 2')}</th>
-                            <th>{t('Email')}</th>
-                            <th>{t('Notes')}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {owners.map((owner, index) => (
-                            <tr key={index} onClick={() => handleRowClick(owner.id)} style={{ cursor: 'pointer' }}>
-                                <td>{owner.first_name}</td>
-                                <td>{owner.last_name}</td>
-                                <td>{owner.phone}</td>
-                                <td>{owner.phone_2}</td>
-                                <td>{owner.email}</td>
-                                <td>{owner.notes}</td>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>{t('First Name')}</th>
+                                <th>{t('Last Name')}</th>
+                                <th>{t('Phone')}</th>
+                                <th>{t('Phone 2')}</th>
+                                <th>{t('Email')}</th>
+                                <th>{t('Notes')}</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>
-            </Row>
-        </Container>
+                        </thead>
+                        <tbody>
+                            {owners.map((owner, index) => (
+                                <tr key={index} onClick={() => handleRowClick(owner.id)} style={{ cursor: 'pointer' }}>
+                                    <td>{owner.first_name}</td>
+                                    <td>{owner.last_name}</td>
+                                    <td>{owner.phone}</td>
+                                    <td>{owner.phone_2}</td>
+                                    <td>{owner.email}</td>
+                                    <td>{owner.notes}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                </Row>
+            </Container >
+        ) :
+            <Container fluid style={{ paddingTop: '90px' }} >
+                <Asset spinner />
+            </Container>
+
     );
 }
 
