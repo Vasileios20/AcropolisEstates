4d9be833c6820fc84cc277140cfa6d3ead7567c5
vasileios20 <vasilis_tsim@outlook.com>
2025-04-09 13:52:44 +0100

    feat: add ShortTermListing and ShortTermImages serializers with upload handling

--- listings/serializers.py
+++ listings/serializers.py
@@ -1,5 +1,8 @@
 from rest_framework import serializers
-from .models import Listing, Images, Amenities, Owner, OwnerFile
+from .models import (
+    Listing, Images, Amenities, Owner, OwnerFile,
+    ShortTermListing, ShortTermImages
+)
 from django.core.files.images import get_image_dimensions
 from .services import upload_to_backblaze
 from django.db.models import Max
@@ -170,10 +173,11 @@ class ListingSerializer(serializers.ModelSerializer):
         # Create the Listing object
         listing = Listing.objects.create(**validated_data)
         listing.amenities.set(amenities)
+        folder = "listings"
 
         # Upload images to Backblaze and create Image objects
         for idx, uploaded_image in enumerate(uploaded_images):
-            filename = generate_unique_filename(listing.id, idx + 1)
+            filename = generate_unique_filename(folder, listing.id, idx + 1)
             try:
                 file_url = upload_to_backblaze(uploaded_image, filename)
                 order = image_orders[idx] if idx < len(image_orders) else idx
@@ -203,6 +207,8 @@ class ListingSerializer(serializers.ModelSerializer):
         if amenities is not None:
             instance.amenities.set(amenities)
 
+        folder = "listings"
+
         # Process uploaded images
         if uploaded_images:
             existing_images = instance.images.all()
@@ -211,7 +217,7 @@ class ListingSerializer(serializers.ModelSerializer):
 
             for idx, uploaded_image in enumerate(uploaded_images):
                 filename = generate_unique_filename(
-                    instance.id, max_order + idx + 1)
+                    folder, instance.id, max_order + idx + 1)
                 try:
                     # Upload to Backblaze and get file URL
                     file_url = upload_to_backblaze(uploaded_image, filename)
@@ -311,6 +317,198 @@ class ListingSerializer(serializers.ModelSerializer):
             "county_id",
             "region_id",
             "listing_owner",
+        ]
+
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        # Sort by order field
+        ret['images'] = sorted(ret['images'], key=lambda x: x['order'])
+        return ret
+
+
+class ShortTermImagesSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the ShortTermImages model.
+
+    This serializer is used to serialize and deserialize ShortTermImages
+    objects. It defines the fields that should be included in the serialized
+    representation of a ShortTermImages object.
+    """
+
+    class Meta:
+        model = ShortTermImages
+        fields = ["id", "listing", "url", "is_first", "order"]
+
+
+class ShortTermListingSerializer(serializers.ModelSerializer):
+    """
+    Serializer class for the ShortTermListing model.
+
+    This serializer is used to serialize and deserialize ShortTermListing
+    objects. It defines the fields that should be included in the serialized
+    representation of a ShortTermListing object.
+    """
+
+    agent_name = serializers.ReadOnlyField(source="agent_name.username")
+    is_owner = serializers.SerializerMethodField()
+    profile_id = serializers.ReadOnlyField(source="agent_name.profile.id")
+    listing_owner = serializers.PrimaryKeyRelatedField(
+        queryset=Owner.objects.all(),
+        allow_null=True,
+        required=False,
+    )
+    images = ShortTermImagesSerializer(
+        many=True,
+        read_only=True,
+    )
+    uploaded_images = serializers.ListField(
+        child=serializers.ImageField(
+            max_length=100000, use_url=False, allow_empty_file=False
+        ),
+        write_only=True,
+        validators=[validate_images],
+        required=False,
+    )
+    amenities = AmenitiesSerializer(
+        many=True,
+        required=False,
+    )
+
+    amenities_ids = serializers.PrimaryKeyRelatedField(
+        many=True,
+        write_only=True,
+        queryset=Amenities.objects.all(),
+        source='amenities'
+    )
+
+    def get_is_owner(self, obj):
+        return self.context["request"].user == obj.agent_name
+
+    def create(self, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images", [])
+        amenities = validated_data.pop('amenities')
+        image_orders = self.context['request'].data.get('image_orders', [])
+        is_first_image_idx = int(
+            self.context['request'].data.get('is_first', 0))
+
+        # Create the Listing object
+        listing = ShortTermListing.objects.create(**validated_data)
+        listing.amenities.set(amenities)
+        folder = "short_term_listings"
+
+        # Upload images to Backblaze and create Image objects
+        for idx, uploaded_image in enumerate(uploaded_images):
+            filename = generate_unique_filename(folder, listing.id, idx + 1)
+            try:
+                file_url = upload_to_backblaze(
+                    uploaded_image, filename)
+                order = image_orders[idx] if idx < len(image_orders) else idx
+                is_first = (is_first_image_idx == idx)
+                ShortTermImages.objects.create(
+                    listing=listing,
+                    url=file_url,
+                    is_first=is_first,
+                    order=order,
+                )
+            except Exception as e:
+                raise serializers.ValidationError(
+                    f"File upload failed: {str(e)}")
+
+        return listing
+
+    def update(self, instance, validated_data):
+        uploaded_images = validated_data.pop("uploaded_images", [])
+        amenities = validated_data.pop('amenities', None)
+        is_first_image_idx = self.context['request'].data.get('is_first', None)
+
+        listing_owner = validated_data.pop("listing_owner", None)
+        if listing_owner is not None:
+            instance.listing_owner = listing_owner
+
+        # Update amenities if provided
+        if amenities is not None:
+            instance.amenities.set(amenities)
+
+        folder = "short_term_listings"
+
+        # Process uploaded images
+        if uploaded_images:
+            existing_images = instance.images.all()
+            max_order = existing_images.aggregate(
+                max_order=Max('order'))['max_order'] or 0
+
+            for idx, uploaded_image in enumerate(uploaded_images):
+                filename = generate_unique_filename(
+                    folder, instance.id, max_order + idx + 1)
+                try:
+                    # Upload to Backblaze and get file URL
+                    file_url = upload_to_backblaze(uploaded_image, filename)
+
+                    # Create a new image with the next order value
+                    ShortTermImages.objects.create(
+                        listing=instance,
+                        url=file_url,
+                        is_first=False,
+                        order=max_order + idx + 1,
+                    )
+                except Exception as e:
+                    raise serializers.ValidationError(
+                        f"File upload failed: {str(e)}")
+
+        if is_first_image_idx is not None:
+            try:
+                is_first_image_idx = int(is_first_image_idx)
+                # Reset all images' `is_first` flag and set the specified one
+                instance.images.update(is_first=False)
+                if 0 <= is_first_image_idx < instance.images.count():
+                    image_to_set_first = instance.images.order_by('order')[
+                        is_first_image_idx
+                    ]
+                    image_to_set_first.is_first = True
+                    image_to_set_first.save()
+            except (ValueError, TypeError, IndexError):
+                raise serializers.ValidationError(
+                    "Invalid value for 'is_first' index.")
+
+        # Call the parent class update method for the rest of the data
+        return super().update(instance, validated_data)
+
+    class Meta:
+        model = ShortTermListing
+        fields = [
+            "id",
+            "agent_name",
+            "description",
+            "description_gr",
+            "address_number",
+            "address_street",
+            "address_street_gr",
+            "municipality",
+            "postcode",
+            "price",
+            "floor_area",
+            "bedrooms",
+            "floor",
+            "kitchens",
+            "bathrooms",
+            "wc",
+            "living_rooms",
+            "created_on",
+            "updated_on",
+            "approved",
+            "longitude",
+            "latitude",
+            "images",
+            "amenities",
+            "municipality_id",
+            "county_id",
+            "region_id",
+            "listing_owner",
+            "uploaded_images",
+            "is_owner",
+            "profile_id",
+            "amenities_ids",
+
         ]
 
     def to_representation(self, instance):
