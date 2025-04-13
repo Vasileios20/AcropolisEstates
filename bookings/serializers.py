388e8ab729be5ab79bfa36aaff97b696179b99b5
vasileios20 <vasilis_tsim@outlook.com>
2025-04-13 11:39:32 +0100

    feat: update ShortTermBookingSerializer to include first and last name fields and enhance validation logic

--- bookings/serializers.py
+++ bookings/serializers.py
@@ -8,12 +8,32 @@ class ShortTermBookingSerializer(serializers.ModelSerializer):
         fields = [
             'id',
             'listing',
-            'name',
+            'first_name',
+            'last_name',
             'email',
             'check_in',
             'check_out',
             'created_at',
             'confirmed',
             'token',
-            'user'
+            'user',
+            'language',
+            'admin_confirmed',
         ]
+
+    def validate(self, data):
+        listing = data['listing']
+        check_in = data['check_in']
+        check_out = data['check_out']
+
+        overlapping = ShortTermBooking.objects.filter(
+            listing=listing,
+            check_in__lt=check_out,
+            check_out__gt=check_in
+        )
+        if self.context['request'].method == 'PUT':
+            overlapping = overlapping.exclude(id=self.instance.id)
+        if overlapping.exists():
+            raise serializers.ValidationError(
+                "These dates are already booked.")
+        return data
